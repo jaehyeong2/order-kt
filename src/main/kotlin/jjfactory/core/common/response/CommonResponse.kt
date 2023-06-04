ab80c58f4ff0e909e3be7f51ede28bcac7393e39
jaehyeong <wogud1514@gmail.com>
2023-06-04 17:16:00 +0900

    refactor : common 패키지 상위로 올리고 CommonResponse추가

---
+++ src/main/kotlin/jjfactory/core/common/response/CommonResponse.kt
@@ -0,0 +1,14 @@
+package jjfactory.core.common.response
+
+import org.springframework.http.HttpStatus
+import java.time.LocalDateTime
+
+data class CommonResponse<T>(
+    var status: Int = HttpStatus.OK.value(),
+    var data: T?,
+    var message: String?,
+    val responseTime: LocalDateTime = LocalDateTime.now(),
+) {
+    constructor(data: T, message: String) : this(HttpStatus.OK.value(), data, message, LocalDateTime.now())
+    constructor(data: T) : this(HttpStatus.OK.value(), data, null, LocalDateTime.now())
+}
